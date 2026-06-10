51ab4a07bf271f15c03e5a8942ab7bdfbed3f7a0
ivasn2 <ivan.zharov0506@gmail.com>
2026-06-10 18:01:18 +0500

    minor improvements

--- src/main/java/app/BankDemo.java
+++ src/main/java/app/BankDemo.java
@@ -1,4 +1,134 @@
 package app;
 
+import model.BankAccount;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class BankDemo {
+    private static Scanner scanner = new Scanner(System.in);
+    private static BankAccount account1;
+    private static BankAccount account2;
+
+    public static void main(String[] args) {
+        while (true) {
+
+            System.out.println("=== БАНК ===");
+            System.out.println("1. Создать счет");
+            System.out.println("2. Пополнить счет");
+            System.out.println("3. Снять деньги");
+            System.out.println("4. Показать данные счета");
+            System.out.println("5. Перевод между счетами");
+            System.out.println("0. Выход");
+
+            try {
+
+                System.out.print("Выберите действие: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+
+                    case 1:
+                        createAccount();
+                        break;
+
+                    case 2:
+                        deposit();
+                        break;
+
+                    case 3:
+                        withdraw();
+                        break;
+
+                    case 4:
+                        printInfo();
+                        break;
+
+                    case 5:
+                        transfer(account2, account1.getBalance());
+                        break;
+
+                    case 0:
+                        System.out.println("Выход из программы...");
+                        return;
+
+                    default:
+                        System.out.println("Такого пункта меню не существует");
+                }
+
+            } catch (InputMismatchException e) {
+
+                System.out.println("Номер пункта меню должен быть числом!");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private static void createAccount() {
+
+        System.out.print("Введите номер счета: ");
+        String number = scanner.nextLine();
+
+        System.out.print("Введите владельца: ");
+        String owner = scanner.nextLine();
+
+        System.out.print("Введите баланс: ");
+        double balance = scanner.nextDouble();
+        scanner.nextLine();
+
+        account1 = new BankAccount(number, owner, balance);
+
+        System.out.println("Счет успешно создан");
+    }
+
+    private static void deposit() {
+
+        if (account1 == null) {
+            System.out.println("Сначала создайте счет");
+            return;
+        }
+
+        System.out.print("Введите сумму: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        account1.deposit(amount);
+        System.out.println("Счет пополнен");
+    }
+
+    private static void withdraw() {
+
+        if (account1 == null) {
+            System.out.println("Сначала создайте счет");
+            return;
+        }
+
+        System.out.println();
+
+    }
+
+    private static void printInfo() {
+        account1.printInfo();
+    }
+
+    private static void transfer(BankAccount target, double amount) {
+        if (account1 == null || account2 == null) {
+            System.out.println("Необходимо создать два счета");
+            return;
+        }
+
+        System.out.print("Введите сумму перевода: ");
+        amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        try {
+            account1.transfer(account2, amount);
+            System.out.println("Перевод выполнен успешно");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
+
--- src/main/java/model/BankAccount.java
+++ src/main/java/model/BankAccount.java
@@ -1,4 +1,68 @@
 package model;
 
 public class BankAccount {
+
+    private final String accountNumber;
+    private String ownerName;
+    private double balance;
+
+
+    public BankAccount(String accountNumber, String ownerName, double balance) {
+        if (balance < 0) {
+            throw new IllegalArgumentException("Ошибка: начальный баланс не может быть отрицательным.");
+        }
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Ошибка: сумма пополнения должна быть больше нуля.");
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Ошибка: сумма снятия должна быть больше нуля.");
+        }
+
+        if (amount > balance) {
+            throw new IllegalArgumentException("Ошибка: нельзя снять больше денег, чем есть на счёте.");
+        }
+        balance -= amount;
+    }
+
+    public void printInfo() {
+        System.out.println("Номер счета: " + accountNumber);
+        System.out.println("Владелец: " + ownerName);
+        System.out.println("Баланс: " + balance);
+    }
+
+
+    public void transfer(BankAccount target, double amount) {
+        if (target == null) {
+            throw new IllegalArgumentException("Ошибка: целевой счет не найден.");
+        }
+        this.withdraw(amount);
+        target.deposit(amount);
+    }
+
 }
